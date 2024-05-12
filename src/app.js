7935f74ff779d3df0a91bff4bdb35e515bd7be30
Dyspersja <dyspersjamusic@gmail.com>
2024-05-12 18:17:43 +0200

    Added JQuery

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const restApiBookRoutes = require('./routes/restApiBookRoutes');
 const graphqlBookRoutes = require('./routes/graphqlBookRoutes');
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', restApiBookRoutes);
