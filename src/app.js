cb0c769fd35438ef29649c16e5dadd4e2d05c20d
Dyspersja <dyspersjamusic@gmail.com>
2024-05-12 17:07:21 +0200

    Removed ejs from application

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 
 const restApiBookRoutes = require('./routes/restApiBookRoutes');
 const graphqlBookRoutes = require('./routes/graphqlBookRoutes');
-const viewBookRoutes = require('./routes/viewBookRoutes');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-api.json');
@@ -13,10 +12,6 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
-app.use('/', viewBookRoutes);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', restApiBookRoutes);
